87da0a7b36fa4fb9d7348875046b4c7d4861f44a
derSchnelleAnton <schweifen-bulldogge.0e@icloud.com>
2025-05-01 15:43:17 +0200

    Test Hello World

--- src/main/java/edu/byteme/Application.java
+++ src/main/java/edu/byteme/Application.java
@@ -18,5 +18,6 @@ public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        System.out.println("Hello World!");
     }
 }
