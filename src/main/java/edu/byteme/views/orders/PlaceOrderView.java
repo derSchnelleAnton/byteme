8c1fd1bf76fcd50477791279d440fa09045d0b9c
Marc Boehme <bm7058@mci4me.at>
2025-05-16 15:44:52 +0200

    PlaceOrderView

--- src/main/java/edu/byteme/views/orders/PlaceOrderView.java
+++ src/main/java/edu/byteme/views/orders/PlaceOrderView.java
@@ -1,4 +1,38 @@
 package edu.byteme.views.orders;
 
-public class PlaceOrderView {
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import edu.byteme.data.entities.MenuItem;
+
+import java.util.List;
+
+/**
+ * PlaceOrderView zeigt eine Liste von MenuItems mit Preis,
+ * basierend auf OrderView.
+ */
+public class PlaceOrderView extends VerticalLayout {
+
+    public PlaceOrderView(List<MenuItem> items) {
+        setSizeFull();
+        setPadding(true);
+        addClassName("place-order-view");
+
+        //Reusing OrderView from Tinsae
+        OrderView orderView = new OrderView(items);
+        orderView.setActionText(""); // Kein Button-Text (Buttons -> ausblenden)
+        orderView.setMenuItemEvent(null); // Keine Aktionen nötig
+        add(orderView);
+
+        HorizontalLayout buttonLayout = new HorizontalLayout();
+        buttonLayout.setSpacing(true);
+        buttonLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+        buttonLayout.setWidthFull();
+
+        Button backButton = new Button("Back");
+        Button orderButton = new Button("Order");
+
+        buttonLayout.add(backButton, orderButton);
+        add(buttonLayout);
+    }
 }
